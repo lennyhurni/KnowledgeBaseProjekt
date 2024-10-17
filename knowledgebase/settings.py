aa780f375a142d74109828f6757e279aa07d6b8c
lennyhurni <148008224+lennyhurni@users.noreply.github.com>
2024-10-17 10:44:27 +0200

    css

--- knowledgebase/settings.py
+++ knowledgebase/settings.py
@@ -99,10 +99,7 @@ USE_TZ = True
 # Statische Dateien (CSS, JavaScript, Bilder)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Medien-Einstellungen
